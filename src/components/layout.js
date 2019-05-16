5c2f1818ac01fcd14a21f9f1b578f92b4fa9c4bc
imhoticon <940709@gmail.com>
2019-05-16 15:25:35 +0900

    experimenting

--- src/components/layout.js
+++ src/components/layout.js
@@ -22,7 +22,7 @@ class Layout extends React.Component {
             style={{
               boxShadow: `none`,
               textDecoration: `none`,
-              color: `pink`,
+              color: `lightblue`,
             }}
             to={`/`}
           >
@@ -42,7 +42,7 @@ class Layout extends React.Component {
             style={{
               boxShadow: `none`,
               textDecoration: `none`,
-              color: `pink`,
+              color: `lightblue`,
             }}
             to={`/`}
           >
